72a84b1af65d37cf71d75b33a6805056cf4ee6c9
Lorena Barbosa <lorenabas@al.insper.edu.br>
2021-12-04 17:45:52 -0300

    java_pesquisar

---
+++ Java_FluxosDeTela/tela_pesquisar.js
@@ -0,0 +1,153 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    botao1= document.querySelector(".bolinha1");
+    Selecionado1 = false;
+
+    botao1.addEventListener("click", function(evento){
+    Selecionado1 = !Selecionado1;
+    if(Selecionado1){
+        botao1.src = "../Imagens/tela_preta.jpg"
+        botao1.style.padding = ".1rem"
+    }
+    else{
+        botao1.src = "../Imagens/tela_branca.jpg"
+        botao1.style.border.color = "#383838"
+    }
+    })
+
+    botao2= document.querySelector(".bolinha2");
+    Selecionado2 = false;
+
+    botao2.addEventListener("click", function(evento){
+    Selecionado2 = !Selecionado2;
+    if(Selecionado2){
+        botao2.src = "../Imagens/tela_preta.jpg"
+        botao2.style.padding = ".1rem"
+    }
+    else{
+        botao2.src = "../Imagens/tela_branca.jpg"
+        botao2.style.border.color = "#383838"
+    }
+    })
+
+    botao3 = document.querySelector(".bolinha3");
+    Selecionado3 = false;
+
+    botao3.addEventListener("click", function(evento){
+    Selecionado3 = !Selecionado3;
+    if(Selecionado3){
+        botao3.src = "../Imagens/tela_preta.jpg"
+        botao3.style.padding = ".1rem"
+    }
+    else{
+        botao3.src = "../Imagens/tela_branca.jpg"
+        botao3.style.border.color = "#383838"
+    }
+    })
+
+    botao4= document.querySelector(".bolinha4");
+    Selecionado4 = false;
+
+    botao4.addEventListener("click", function(evento){
+    Selecionado4 = !Selecionado4;
+    if(Selecionado4){
+        botao4.src = "../Imagens/tela_preta.jpg"
+        botao4.style.padding = ".1rem"
+    }
+    else{
+        botao4.src = "../Imagens/tela_branca.jpg"
+        botao4.style.border.color = "#383838"
+    }
+    })
+
+    botao5= document.querySelector(".bolinha5");
+    Selecionado5= false;
+
+    botao5.addEventListener("click", function(evento){
+    Selecionado5 = !Selecionado5;
+    if(Selecionado5){
+        botao5.src = "../Imagens/tela_preta.jpg"
+        botao5.style.padding = ".1rem"
+    }
+    else{
+        botao5.src = "../Imagens/tela_branca.jpg"
+        botao5.style.border.color = "#383838"
+    }
+    })
+
+    botao6= document.querySelector(".bolinha6");
+    Selecionado6 = false;
+
+    botao6.addEventListener("click", function(evento){
+    Selecionado6 = !Selecionado6;
+    if(Selecionado6){
+        botao6.src = "../Imagens/tela_preta.jpg"
+        botao6.style.padding = ".1rem"
+    }
+    else{
+        botao6.src = "../Imagens/tela_branca.jpg"
+        botao6.style.border.color = "#383838"
+    }
+    })
+
+    botao7= document.querySelector(".bolinha7");
+    Selecionado7 = false;
+
+    botao7.addEventListener("click", function(evento){
+    Selecionado7 = !Selecionado7;
+    if(Selecionado7){
+        botao7.src = "../Imagens/tela_preta.jpg"
+        botao7.style.padding = ".1rem"
+    }
+    else{
+        botao7.src = "../Imagens/tela_branca.jpg"
+        botao7.style.border.color = "#383838"
+    }
+    })
+
+    botao8= document.querySelector(".bolinha8");
+    Selecionado8 = false;
+
+    botao8.addEventListener("click", function(evento){
+    Selecionado8 = !Selecionado8;
+    if(Selecionado8){
+        botao8.src = "../Imagens/tela_preta.jpg"
+        botao8.style.padding = ".1rem"
+    }
+    else{
+        botao8.src = "../Imagens/tela_branca.jpg"
+        botao8.style.border.color = "#383838"
+    }
+    })
+
+    botao9= document.querySelector(".bolinha9");
+    Selecionado9 = false;
+
+    botao9.addEventListener("click", function(evento){
+    Selecionado9 = !Selecionado9;
+    if(Selecionado9){
+        botao9.src = "../Imagens/tela_preta.jpg"
+        botao9.style.padding = ".1rem"
+    }
+    else{
+        botao9.src = "../Imagens/tela_branca.jpg"
+        botao9.style.border.color = "#383838"
+    }
+    })
+
+    botao10= document.querySelector(".bolinha10");
+    Selecionado10 = false;
+
+    botao10.addEventListener("click", function(evento){
+    Selecionado10 = !Selecionado10;
+    if(Selecionado10){
+        botao10.src = "../Imagens/tela_preta.jpg"
+        botao10.style.padding = ".1rem"
+    }
+    else{
+        botao10.src = "../Imagens/tela_branca.jpg"
+        botao10.style.border.color = "#383838"
+    }
+    })
+
+})
